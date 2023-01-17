f2349ea614d95bbf7532a28e2c872da637934623
Thomas Bono <thomasbono.com@gmail.com>
2023-01-17 18:03:20 -0500

    installed the DS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    css: [
+        '@nypublicradio/nypr-design-system-vue3/src/assets/themes/gothamist/fonts/fonts.css',
+        '@nypublicradio/nypr-design-system-vue3/src/assets/themes/gothamist/gothamist.min.css',
+        'primevue/resources/primevue.min.css',
+        'primeicons/primeicons.css',
+    ],
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    // besure to mirror theses imports in the vitest.config.ts
+                    additionalData: `@import "@nypublicradio/nypr-design-system-vue3/src/assets/themes/gothamist/breakpoints.scss"; @import "@nypublicradio/nypr-design-system-vue3/src/assets/themes/gothamist/_mixins.scss"; @import "@nypublicradio/nypr-design-system-vue3/src/assets/themes/gothamist/typography.scss"; @import "~/assets/scss/global.scss";`,
+                },
+            },
+            postcss: {
+                plugins: [
+                    {
+                        postcssPlugin: 'internal:charset-removal',
+                        AtRule: {
+                            charset: (atRule) => {
+                                if (atRule.name === 'charset') {
+                                    atRule.remove();
+                                }
+                            }
+                        }
+                    }
+                ]
+            }
+        },
+    },
     runtimeConfig: {
         public: {
             LIVESTREAM_URL: process.env['LIVESTREAM_URL'] || 'https://api.demo.nypr.digital/api/v4/whats_on/',
