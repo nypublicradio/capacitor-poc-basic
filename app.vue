822505643eaadb6a9ac9b8d348a612a4ced8cbc3
Thomas Bono <thomasbono.com@gmail.com>
2023-02-09 17:47:16 -0500

    app.vue update

--- app.vue
+++ app.vue
@@ -2,10 +2,10 @@
 import { IonApp, IonRouterOutlet } from '@ionic/vue'
 </script>
 <template>
-  <ion-app>
-    <NuxtLayout>
-      <NuxtPage />
-      <!-- <ion-router-outlet class="relative"></ion-router-outlet> -->
-    </NuxtLayout>
-  </ion-app>
+  <!-- <ion-app> -->
+  <NuxtLayout>
+    <NuxtPage />
+    <!-- <ion-router-outlet class="relative"></ion-router-outlet> -->
+  </NuxtLayout>
+  <!-- </ion-app> -->
 </template>
